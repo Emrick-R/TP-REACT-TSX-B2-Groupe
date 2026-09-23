90ec0b7a579297c77ebd16f4e6f3fa55259c8f87
Emrick <emrick.rivet@ynov.com>
2026-09-23 16:17:30 +0200

    feat recipe: Ajout des champs supplémentaire selon jsondummy

--- src/pages/Recette.tsx
+++ src/pages/Recette.tsx
@@ -53,6 +53,9 @@ function Recette() {
                     <></>
                 }</h1>
                 <p className="recipe-meta">{`Temps de préparation : ${recipe.prepTimeMinutes} minutes \| Temps de cuissons : ${recipe.cookTimeMinutes} minutes`}</p>
+                <p className="recipe-meta">{`Nombre de part : ${recipe.servings} par personne (${recipe.caloriesPerServing} cal/pers)\| Difficulté : ${recipe.difficulty}`}</p>
+                <p className="recipe-meta">{`Recette ${recipe.cuisine} pour le `}{recipe.mealType.map((type) => <span>{type} </span>)}{`\| Tags : `}{recipe.tags.map((tag) => <span>{tag} </span>)}</p>
+                <p className="recipe-meta">{`note : ${recipe.rating} \| nombre d'avis : ${recipe.reviewCount}`}</p>
                 <div className="recipe-columns">
                     <div>
                         <h2>Ingrédients</h2>
